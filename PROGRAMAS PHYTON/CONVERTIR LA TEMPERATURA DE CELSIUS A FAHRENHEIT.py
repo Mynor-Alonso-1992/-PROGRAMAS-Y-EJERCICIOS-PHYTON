de5d19803c4f348a08cdb0c2f7bdf5a6d6478108
Mynor-Alonso-1992 <136778116+Mynor-Alonso-1992@users.noreply.github.com>
2023-09-07 01:27:48 -0600

    Add files via upload

---
+++ PROGRAMAS PHYTON/CONVERTIR LA TEMPERATURA DE CELSIUS A FAHRENHEIT.py
@@ -0,0 +1,9 @@
+# Pyton Program to convert temperature in celsius to fahrenheit
+
+# change this value for a different result
+celsius = 37.5
+
+# calculte fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+
+print('%0.1f degree Celsius is equial to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
